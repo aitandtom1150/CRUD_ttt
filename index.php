b4a927d2251b89c1fc76f1402f7820827579ff32
aitsada <aitandtom1150@gmail.com>
2025-05-27 03:03:52 +0700

    a

--- index.php
+++ index.php
@@ -9,25 +9,7 @@
 
   <body>
 
-    <div class="modal fade" tabindex="-1" id="addNewUserModal">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-header">Add new user</h5>
-                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="close"></button>
-                </div>
-                <div class="modal-body">
-                    <form id="add-user-form" class="p-2" novalidate>
-                        <div class="row mb-3 gx-3">
-                            <div class="col">
-                                <input type="text" name="fname" class="form-com>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- menu -->
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
