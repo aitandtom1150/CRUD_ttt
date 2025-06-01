e85419c641a49dc5102fd2f77d6514e56e581ff6
Aitsada <aitandtom1150@gmail.com>
2025-06-02 02:12:08 +0700

    ait

--- test.java
+++ test.java
@@ -1,7 +1,16 @@
-class test {
-    public static void main(String[]args){
-        
+import javax.swing.*;
+import java.awt.*;
 
-       System.out.println("Aitsada suwannato");
+class test {
+
+    public static void main(String[]args){
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        frame.setTitle("Aitsada");
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("ait.png"));
+ 
     }
 }
