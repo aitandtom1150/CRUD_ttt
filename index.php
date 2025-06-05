38f77d1a1b494375cc37c004e252b10ceb39e6f3
aitsada <aitandtom1150@gmail.com>
2025-06-05 22:52:11 +0700

    add thing

--- index.php
+++ index.php
@@ -1,41 +1,128 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD</title>
 
-    <link href="style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-   
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRUD</title>
+
+  <link href="style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
 </head>
 
-  <body style="background:grey">
-    
-    <!-- top bar -->
+<body style="background:grey">
 
-    <!-- <div style="display: flex; gap: 50rem;">
+  <!-- top bar -->
+
+  <!-- <div style="display: flex; gap: 50rem;">
         <div><button style="border: none; background-color: transparent; cursor: pointer;"><a href="">Home</a></button></div>
         <div>Mod</div>
         <div>Profile</div>
     </div>
      -->
 
-    <!-- data -->
-    <div>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Kind</th>
-            <th>Part</th>
-            <th>Protein</th>
-            <th>kcal</th>
-          </tr>
-        </thead>
-      </table>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-  </body>
+  <!-- add new list Start -->
+  <div class="modal fade" tabinbox="-1" id="addNewListModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <h5 class="modal-header">Add New List</h5>
+        <button type="button" class="btn-close" data-dismiss="modal">New List</button>
+      </div>
+      <div class="modal-body">
+        <form id="add-list-form" class="p-2" novalidate>
+          <div class="row mb-3 gx-3">
+            <div class="col">
+              <input type="text" name="kind" class="form-control form-control-lg" placeholder="Enter Kind" required>
+              <div class="invalid-feedback">enter kind</div>
+            </div>
+            <div class="col">
+              <input type="text" name="part" class="form-control form-control-lg" placeholder="Enter Part" required>
+              <div class="invalid-feedback">enter kind</div>
+            </div>
+            <div class="col">
+              <input type="text" name="protein" class="form-control form-control-lg" placeholder="Enter Protein"
+                required>
+              <div class="invalid-feedback">enter kind</div>
+            </div>
+            <div class="col">
+              <input type="text" name="kcal" class="form-control form-control-lg" placeholder="Enter kcal">
+              <div class="invalid-feedback">enter kind</div>
+            </div>
+            <div class="mb-3">
+              <input type="submit" value="Add list" class="btn btn-primary btn-block btn-lg" id="add-list-btn">
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </div>
+  </div>
+  <!-- add new list End -->
+
+  <!-- Edit List Modal Start -->
+  <div class="modal fade" tabindex="-1" id="editListModal">
+    <div class="moda-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-header">Edit This List</h5>
+          <button type="button" class="btn-close" data-></button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- data -->
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-lg-12 d-flex justify-content-between align-items-center">
+        <div>
+          <h4 class="text-danger">All List in database</h4>
+        </div>
+        <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#addNewUserModal">Add New
+          List</button>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-lg-12">
+      <div id="showAlert"></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="table-responsive">
+        <table class="table table-striped table-boredered text-center">
+          <div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Kind</th>
+                  <th>Part</th>
+                  <th>Protein</th>
+                  <th>kcal</th>
+                </tr>
+              </thead>
+              <tbody>
+
+              </tbody>
+            </table>
+          </div>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+    crossorigin="anonymous"></script>
+  <script src="main.js"></script>
+</body>
+
 </html>
---
+++ main.js
@@ -0,0 +1,41 @@
+const addForm = document.getElementById("add-list-btn");
+const showAlert = document.getElementById("")
+const addModal = new bootstrap.Modal(doncument.getElementByid("addNewListModal"));
+const tbody = document.querySelector("tbody");
+
+addForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addForm);
+    formData.append("add", 1);
+
+    if (addForm.checkValidity() === false) {
+        e.preventDefault();
+        e.stopPropagation();
+        addForm.classList.add("was-validated");
+        return false;
+    } else {
+        document.getElementById("add-list-btn").value ="Pease wait...";
+
+        const data = await fetch("action.php", {
+            method: "POST",
+            body: formData
+        })
+        const response = await data.text();
+        showAlert.innerHTML = response;
+        document.getElementById("add-list-btn").value = "Add List";
+        addForm.reset();
+        addForm.classList.remove("was-validated");
+        addModal.hide();
+        fetchAllList();
+    }
+})
+
+const fetchAllList = async () => {
+    const data = await fetch("action.php?read=1", {
+        method: "GET"
+    })
+    const response = await data.text();
+    tbody.innerHTML = response;
+}
+fetchAllList();
